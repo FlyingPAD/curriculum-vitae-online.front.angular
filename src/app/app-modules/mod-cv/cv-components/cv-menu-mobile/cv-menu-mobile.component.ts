0b756049654f7422ff896d7540cee03328175fd6
FlyingPAD <tonyvan@live.fr>
2023-05-04 04:37:06 +0200

    menu optimization

--- src/app/app-modules/mod-cv/cv-components/cv-menu-mobile/cv-menu-mobile.component.ts
+++ src/app/app-modules/mod-cv/cv-components/cv-menu-mobile/cv-menu-mobile.component.ts
@@ -8,24 +8,81 @@ import { CvMenuService } from '../../cv-services/cv-menu.service';
 })
 export class CvMenuMobileComponent 
 {
-  // Properties :
+  // ----------------------------------------------------------------------
+  // - Properties :
 
-  height : number
+  windowHeight : number
+  menuHeight : number
+  topPosition : number
+  pageHeight : number
+  menuHalf : number // If menu > window
 
-  // Constructor :
+  // ----------------------------------------------------------------------
+  // - Constructor :
 
   constructor(public menuService : CvMenuService)
   {
-    this.height = 0
+    this.pageHeight = 0
+    this.menuHeight = 0
+    this.windowHeight = 0
+    this.topPosition = 15
+    this.menuHalf = 0
   }
 
-  // Methods
+  // ----------------------------------------------------------------------
+  // - Methods
 
   @HostListener('window:scroll', [])
   onScroll() 
   {
-    console.log(window.scrollY)
+    this.windowHeight = window.innerHeight
+    this.menuHeight = document.querySelector('#cv-menu-mobile')?.clientHeight || 0
+    this.pageHeight = document.body.scrollHeight
+    this.menuHalf = this.menuHeight - this.windowHeight
 
-    this.height = window.scrollY
+    console.log('-----------------------------------------')
+    console.log(' - Page Height : ' + this.pageHeight)
+    console.log(' - Menu Height : ' + this.menuHeight)
+    console.log(' - Window Height : ' + this.windowHeight)
+    console.log(' - Menu Top Position : ' + this.topPosition)
+    console.log(' - Current Scroll Position : ' + window.scrollY)
+    console.log(' - Menu Half : ' + this.menuHalf)
+
+    // When scrolling Up :
+
+    if(window.scrollY < this.topPosition - 75) 
+    {
+      if(window.scrollY <= 0 || window.scrollY < 50)
+      {
+        this.topPosition = 15
+      }
+      else this.topPosition = window.scrollY + 15
+    }
+
+    // When scrolling Down :
+
+    if(this.menuHalf <= 0) // If Menu < Window
+    {
+      if(window.scrollY > this.topPosition + 150) // When Scrolling Down : 
+      {
+        if(window.scrollY > this.pageHeight - this.menuHeight )
+        {
+          this.topPosition = this.pageHeight - this.menuHeight - 15
+        }
+        else this.topPosition = window.scrollY + 15
+      }
+    }
+
+    if(this.menuHalf > 0) // If Menu > Window
+    { 
+      if(window.scrollY > this.topPosition + this.menuHalf + 150) // When Scrolling Down : 
+      {
+        if(window.scrollY > this.pageHeight - this.menuHeight )
+        {
+          this.topPosition = this.pageHeight - this.menuHeight - 15
+        }
+        else this.topPosition = window.scrollY + 15
+      }
+    }
   }
 }
